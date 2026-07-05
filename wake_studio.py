58ceb7c6ffebd6e5a206a836dca756e794209b10
Kira <arsingh1131@gmail.com>
2026-07-05 04:38:15 +0000

    wake_studio.yml: fix indentation - use script file instead of inline python

--- wake_studio.py
+++ wake_studio.py
@@ -1,9 +1,9 @@
 import os
 import lightning_sdk
 
-os.environ['LIGHTNING_USER_ID'] = os.environ.get('LIGHTNING_USER_ID', '')
-os.environ['LIGHTNING_API_KEY'] = os.environ.get('LIGHTNING_API_KEY', '')
+os.environ["LIGHTNING_USER_ID"] = os.environ.get("LIGHTNING_USER_ID", "")
+os.environ["LIGHTNING_API_KEY"] = os.environ.get("LIGHTNING_API_KEY", "")
 
-studio = lightning_sdk.Studio(name='hermes-1')
+studio = lightning_sdk.Studio(name="hermes-1")
 studio.start()
-print('Studio wake command sent.')
+print("Studio wake command sent.")
